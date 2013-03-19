4d00295cd49d98a2c0a74860c8614c4aa499eb60
Paul Fenwick <pjf@perltraining.com.au>
2013-03-20 00:16:20 +1100

    Basic add task functionality.

--- lib/WebService/HabitRPG.pm
+++ lib/WebService/HabitRPG.pm
@@ -5,6 +5,8 @@ use warnings;
 use autodie;
 use Moo;
 use WWW::Mechanize; # Probably overkill
+use Method::Signatures;
+use JSON::Any;
 
 # ABSTRACT: Perl interface to the HabitRPG API
 
@@ -19,6 +21,8 @@ has 'user_id'   => (is => 'ro'); # aka x-api-user
 has 'agent'     => (is => 'rw');
 
 use constant URL_BASE => 'http://habitrpg.com/api/v1';
+
+my $json = JSON::Any->new;
 
 sub BUILD {
     my ($self, $args) = @_;
@@ -36,19 +40,16 @@ sub BUILD {
     return;
 }
 
-sub user {
-    my ($self) = @_;
+method user() {
 
     my $req = $self->_request('GET', '/user');
 
     my $response = $self->agent->request( $req );
 
     return $response->decoded_content;
-
 }
 
-sub user_tasks {
-    my ($self) = @_;
+method user_tasks() {
 
     my $req = $self->_request('GET', '/user/tasks');
 
@@ -57,8 +58,7 @@ sub user_tasks {
     return $response->decoded_content;
 }
 
-sub get_task {
-    my ($self, $task_id) = @_;
+method get_task($task_id) {
 
     my $req = $self->_request('GET', "/user/task/$task_id");
 
@@ -67,8 +67,31 @@ sub get_task {
     return $response->decoded_content;
 }
 
-sub _request {
-    my ($self, $type, $url) = @_;
+method new_task(
+    :$type! where qr{ habit | daily | todo | reward }x,
+    :$text!,
+    :$completed,
+    :$value = 0,
+    :$note = ''
+) {
+
+    my $payload = $json->encode({
+        type      => $type,
+        text      => $text,
+        completed => $completed,
+        value     => $value,
+        note      => $note,
+    });
+
+    my $req = $self->_request('POST', '/user/task');
+
+    $req->content( $payload );
+
+    return $self->agent->request( $req )->decoded_content;
+
+}
+
+method _request($type, $url) {
 
     my $req = HTTP::Request->new( $type, URL_BASE . $url );
     $req->header( 'Content-Type' => 'application/json');
